7a301cc0b7e38adcf5f70e73f99e6a4b2cdf5d8a
rxu-plaid <129425432+rxu-plaid@users.noreply.github.com>
2024-09-18 11:50:41 -0400

    Update transcoder.go

--- transcoder.go
+++ transcoder.go
@@ -1094,6 +1094,7 @@ func (w *responseWriter) WriteHeader(statusCode int) {
 		w.op.client.respCompression = respCompression
 		w.op.server.respCompression = respCompression
 	}
+	fmt.Println(respMeta)
 	if respMeta.codec != "" && respMeta.codec != w.op.server.codec.Name() &&
 		!restHTTPBodyResponse(w.op) {
 		// unexpected content-type for reply
